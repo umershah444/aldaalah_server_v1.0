8c735036b9359df353839cc16835224e27239be2
umershah444 <umershah444@gmail.com>
2017-08-08 15:44:39 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var app = require('express')();
 var http = require('http').Server(app);
  require('./routes/routes.js')(app);
 
-http.listen(3000, function(){
+http.listen(8080, function(){
   console.log('listening on *:3000');
 });
