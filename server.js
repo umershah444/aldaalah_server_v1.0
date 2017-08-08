2c70db33b3c897d6be133fdad458d9a9703125ae
Saif ullah khan <saifullah.khan@zipycare.com>
2017-08-08 16:06:50 +0500

    server port update 4

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var app = require('express')();
 var http = require('http').Server(app);
  require('./routes/routes.js')(app);
+var port = process.env.PORT || 3000;
 
-http.listen(process.env.PORT, function(){
-  console.log('listening on *:3000');
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+http.listen(port, function(){
+  console.log('listening on *:'+port);
 });
